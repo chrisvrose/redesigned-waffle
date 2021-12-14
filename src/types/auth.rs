4ed516b84d47320e80a7cff17c5e0c038d9334ac
Christopher Rose <christophervrose@gmail.com>
2021-12-14 11:12:50 +0530

    [add] jwt handler functions

---
+++ src/types/auth.rs
@@ -0,0 +1,67 @@
+use jsonwebtoken::{decode, encode, errors::Error as JWTError, DecodingKey, Validation};
+use serde::{Deserialize, Serialize};
+use std::time::{UNIX_EPOCH};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct AuthTokenData {
+    uid: i32,
+    exp: usize,
+}
+
+/// add days to current timestamp
+fn add_days_to_epoch(expdays:u8)->u64{
+    let origin = UNIX_EPOCH;
+    const SECS_IN_DAYS:u64 = 24*60*60;
+    let duration = origin.elapsed().expect("We're still living in the past");
+    
+    duration.as_secs()+(expdays as u64)*SECS_IN_DAYS
+}
+
+pub fn issue_jwt(key: &String, uid: i32,expdays:u8) -> Result<String, JWTError> {
+    let key = key.as_bytes();
+    
+    let exp_time = add_days_to_epoch(expdays); 
+    let data = AuthTokenData { uid, exp: exp_time as usize };
+
+    let claims = encode(
+        &jsonwebtoken::Header::default(),
+        &data,
+        &jsonwebtoken::EncodingKey::from_secret(key),
+    );
+
+    claims
+}
+
+pub fn validate_jwt(key: &String, token: &String) -> Result<AuthTokenData, JWTError> {
+    let key = key.as_bytes();
+    
+
+    let res = decode::<AuthTokenData>(
+        &token,
+        &DecodingKey::from_secret(key),
+        &Validation::default(),
+    )?;
+
+    Ok(res.claims)
+}
+
+#[cfg(test)]
+pub mod tests {
+    use super::*;
+    use jsonwebtoken::errors::Error as JWTError;
+    #[test]
+    pub fn test_jwt() -> Result<(), JWTError> {
+        let secret = String::from("mySecret");
+        let jwt = issue_jwt(&secret, 23,5)?;
+
+        let uid = validate_jwt(&secret, &jwt)?;
+
+        assert_eq!(
+            uid.uid, 23,
+            "Testing that we get back same uid, had expected {} but got {}",
+            23, uid.uid
+        );
+
+        Ok(())
+    }
+}
--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,20 +1,19 @@
-use serde::{Serialize};
+use serde::Serialize;
 use sqlx::PgPool;
-
-
-pub struct AppData{
-    pub salt:String,
-    pub pool:PgPool
+pub mod auth;
+pub struct AppData {
+    pub salt: String,
+    pub pool: PgPool,
 }
 
 
 #[derive(Serialize)]
-pub struct JsonResponse<T>{
-    pub ok:bool,
-    pub response:T
+pub struct JsonResponse<T> {
+    pub ok: bool,
+    pub response: T,
 }
-impl<T> JsonResponse<T>{
-    pub fn new(ok:bool,response:T)->JsonResponse<T>{
-        JsonResponse{ok,response}
+impl<T> JsonResponse<T> {
+    pub fn new(ok: bool, response: T) -> JsonResponse<T> {
+        JsonResponse { ok, response }
     }
 }
